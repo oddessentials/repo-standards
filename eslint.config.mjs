e5891f6c0bcfacee1aca3db8f82a89358cfb8639
PM Palles <pallespm@upmc.edu>
2025-12-07 21:05:09 -0500

    fix: linting config needed for ci

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  {
+    files: ["**/*.ts"],
+    ignores: ["dist/**", "node_modules/**"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+        // No "project" here so we don't require type-aware linting
+      },
+      globals: {
+        ...globals.node
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin
+    },
+    rules: {
+      // Basic sane defaults – you can tweak later
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", ignoreRestSiblings: true }
+      ]
+    }
+  }
+];
